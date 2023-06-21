b790282e5eb4de3d9bce2d4beb16e79936fe407d
Edouard Yonga <ededwardyonga@gmail.com>
2023-06-20 20:49:01 -0400

    feat(ui): header design

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,31 +1,108 @@
-import * as React from 'react';
+import Image from 'next/image';
 
-import UnstyledLink from '@/components/links/UnstyledLink';
-
-const links = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
+const navigation = [
+  { name: 'Things to Do', href: '#thingsToDo' },
+  { name: 'Restaurants', href: '#' },
+  { name: 'Rental car', href: '#' },
+  { name: 'Gues House', href: '#' },
 ];
 
-export default function Header() {
+export default function Example() {
   return (
-    <header className='sticky top-0 z-50 bg-white'>
-      <div className='layout flex h-14 items-center justify-between'>
-        <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
-          Home
-        </UnstyledLink>
-        <nav>
-          <ul className='flex items-center justify-between space-x-4'>
-            {links.map(({ href, label }) => (
-              <li key={`${href}${label}`}>
-                <UnstyledLink href={href} className='hover:text-gray-600'>
-                  {label}
-                </UnstyledLink>
-              </li>
+    <div className='mx-5 bg-white'>
+      <header className='absolute inset-x-0 top-0 z-50 mx-5'>
+        <nav
+          className='flex items-center justify-between p-6 lg:px-8'
+          aria-label='Global'
+        >
+          <div className='flex lg:flex-1'>
+            <a href='#' className='-m-1.5 p-1.5'>
+              <span className='text-2xl font-bold text-[#9089fc]'>
+                PinPoint.
+              </span>
+              {/* <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt=""
+              /> */}
+            </a>
+          </div>
+          <div className='hidden lg:flex lg:gap-x-12'>
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className={`${
+                  item.href === '#thingsToDo'
+                    ? 'font-semibold text-blue-800'
+                    : ''
+                } text-sm font-semibold leading-6 text-gray-900 hover:text-blue-800`}
+              >
+                {item.name}
+              </a>
             ))}
-          </ul>
+          </div>
         </nav>
+      </header>
+
+      <div className='relative isolate px-6 pt-14 lg:px-8'>
+        <div
+          className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
+          aria-hidden='true'
+        >
+          <div
+            className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
+        </div>
+        <div className=' flex justify-between gap-2 py-32 sm:py-48 lg:py-56'>
+          <div className='max-w-2xl'>
+            <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
+              Things to do in Yaounde
+            </h1>
+            <p className='mt-6 text-lg leading-8 text-gray-600'>
+              Discover Yaoundé, Cameroon's vibrant capital! Explore museums,
+              markets, and architectural gems. Immerse yourself in local culture
+              with music, dance, and delicious cuisine. Unforgettable adventures
+              await in Yaoundé!
+            </p>
+            <div className='mt-10 gap-x-6'>
+              <a
+                href='#'
+                className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+              >
+                Get started
+              </a>
+            </div>
+          </div>
+          <div className=''>
+            {/* <HeroSearch className='w-50 h-50 fill-current' /> */}
+            <Image
+              src='/images/location_search.png'
+              width={500}
+              height={500}
+              sizes='(max-width: 768px) 100vw'
+              alt='Picture of the author'
+            />
+          </div>
+          <div></div>
+        </div>
+        <div
+          className='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]'
+          aria-hidden='true'
+        >
+          <div
+            className='relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]'
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
+        </div>
       </div>
-    </header>
+    </div>
   );
 }
